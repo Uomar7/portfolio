fed23a2c9f3a3e8cefc3397582fc4951d174fe9a
Uomar <uomarearlie7@gmail.com>
2019-09-15 23:33:10 +0300

    added a static parallax effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').hover();   
-  });
+  $('[data-toggle="tooltip"]').hover(); 
+});
+
+const parallax = document.getElementById('introd');
+window.addEventListener("scroll", function(){
+  let offset = window.pageYOffset;
+  console.log(offset);
+  
+  parallax.style.backgroundPositionY = offset * 0.7 + 'px';
+})
